f4b6cf150247d2ce836c5a934b2c89578e5372d8
Mariam Abdelgalil <88682947+mariamabdelgalil@users.noreply.github.com>
2025-08-04 00:24:46 +0300

    Add client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React,  {Fragment} from "react";
+import InputTodo from "./components/InputTodo";
+import ListTodos from "./components/ListTodos";
+function App() {
+  return (
+    <Fragment>
+      <div className = "container"><InputTodo/> 
+      <ListTodos/>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
